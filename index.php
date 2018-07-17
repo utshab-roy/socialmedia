b963f2e6dac6f3c3b7309e1ec3130bdfd41017b1
Utshab Roy <utshab.roy@gmail.com>
2018-07-17 14:34:59 +0600

    first page built, gitignore added

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
+    <title>Home</title>
+</head>
+<body class="text-center">
+<div class="container">
+    <div class="row">
+<!--        <div class="col-3"></div>-->
+        <div class="col-md-6 offset-md-3">
+            <form class="form-signin">
+                <h1 class="display-3">Social Media</h1>
+<!--                <a href="../socialmedia"><img src="https://image.ibb.co/eiByzJ/logo.jpg" alt="logo" width ="500px" border="0"></a>-->
+                <h1 class="h3 mb-3 mt-3 font-weight-normal">Please sign in</h1>
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <p class="mt-3">Still not a member ? Please Sign Up</p>
+                <button class="btn btn-lg btn-outline-primary btn-block" type="submit">Sign up</button>
+                <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+            </form>
+        </div>
+<!--        <div class="col-3"></div>-->
+    </div>
+
+</div>
+
+</body>
+</html>
